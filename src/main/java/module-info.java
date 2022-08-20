e814e0bb9463cd76a3b88b803dcf3f2bae7a521a
Stefan Prelle <stefan@rpgframework.de>
2022-08-20 14:58:23 +0200

    Add assembly for updater

---
+++ src/main/java/module-info.java
@@ -0,0 +1,22 @@
+open module de.rpgframework.shadowrun6.commlink6 {
+	exports de.rpgframework.shadowrun6.comlink;
+	exports de.rpgframework.shadowrun6.comlink.pages;
+
+	requires com.gluonhq.attach.browser;
+	requires com.gluonhq.attach.util;
+	requires de.rpgframework.core;
+	requires de.rpgframework.eden.client;
+	requires de.rpgframework.eden.client.jfx;
+	requires de.rpgframework.javafx;
+	requires de.rpgframework.rules;
+	requires de.rpgframework.shadowrun6.chargen.jfx;
+	requires de.rpgframework.shadowrun6.core;
+	requires de.rpgframework.shadowrun6.data;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.extensions;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires shadowrun.common;
+	requires shadowrun.common.chargen.jfx;
+}
