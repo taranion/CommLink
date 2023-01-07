e11d99e204b42d2c55f60067a4d8ce2956afe6be
Stefan Prelle <stefan@rpgframework.de>
2023-01-07 16:59:30 +0100

    Make loglevel configurable via file

--- src/main/java/de/rpgframework/shadowrun6/comlink/CustomLoggerFinder.java
+++ src/main/java/de/rpgframework/shadowrun6/comlink/CustomLoggerFinder.java
@@ -1,10 +1,18 @@
 package de.rpgframework.shadowrun6.comlink;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 import java.lang.System.LoggerFinder;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author prelle
@@ -13,6 +21,42 @@ import java.util.Map;
 public class CustomLoggerFinder extends LoggerFinder {
 
 	private static Map<String, Logger> loggerByName = new HashMap<>();
+	private static Map<String, Level> levelByPrefix = new HashMap<>();
+
+	//-------------------------------------------------------------------
+	static {
+		InputStream ins = ClassLoader.getSystemResourceAsStream("loglevel.properties");
+		if (ins!=null) {
+			try {
+				BufferedReader read = new BufferedReader(new InputStreamReader(ins));
+				while (true) {
+					String line = read.readLine();
+					if (line==null) break;
+					if (line.startsWith("#")) continue;
+					int pos = line.indexOf("=");
+					if (pos<0) continue;
+					String key = line.substring(0, pos).trim();
+					Level level = Level.valueOf(line.substring(pos+1, line.length()).trim());
+					levelByPrefix.put(key, level);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	//-------------------------------------------------------------------
+	private static Level getLevelFor(String name) {
+		List<String> keys = levelByPrefix.keySet().stream().filter(key -> name.startsWith(key)).collect(Collectors.toList());
+		Collections.sort(keys, new Comparator<String>() {
+			public int compare(String o1, String o2) {
+				return -Integer.compare(o1.length(), o2.length());
+			}
+		});
+		if (keys.isEmpty())
+			return Level.INFO;
+		return levelByPrefix.get(keys.get(0));
+	}
 
 	//-------------------------------------------------------------------
 	/**
@@ -23,24 +67,8 @@ public class CustomLoggerFinder extends LoggerFinder {
 		if (loggerByName.containsKey(name))
 			return loggerByName.get(name);
 		ConsoleLogger ret = null;
-		if (name.startsWith("prelle")) {
-			ret = new ConsoleLogger(name, Level.WARNING);
-		} else if (name.startsWith("javafx")) {
-			ret = new ConsoleLogger(name, Level.ERROR);
-		} else if (name.startsWith("de.rpgframework.genericrpg.items") || name.startsWith("de.rpgframework.shadowrun6.items")) {
-			ret = new ConsoleLogger(name, Level.WARNING);
-		} else if (name.startsWith("de.rpgframework.shadowrun.chargen.enhance") || name.startsWith("de.rpgframework.shadowrun6.chargen6.skill")) {
-			ret = new ConsoleLogger(name, Level.DEBUG);
-		} else if (name.startsWith("de.rpgframework.shadowrun.chargen") || name.startsWith("de.rpgframework.shadowrun6.chargen")) {
-			ret = new ConsoleLogger(name, Level.INFO);
-		} else if (name.contains(".proc")) {
-			ret = new ConsoleLogger(name, Level.WARNING);
-//		} else if (name.equals("de.rpgframework.shadowrun6.chargen.gen.priority.attrib")) {
-//			ret = new ConsoleLogger(name, Level.TRACE);
-		} else {
-//			System.err.println("getLogger: "+name);
-			ret = new ConsoleLogger(name, Level.INFO);
-		}
+
+		ret = new ConsoleLogger(name, getLevelFor(name));
 		loggerByName.put(name, ret);
 
 		return ret;
