758dfabd8db7c85b5efaaaec5a33fc529f1c1d89
Stefan Prelle <stefan@rpgframework.de>
2023-10-16 22:06:03 +0200

    Remove data

--- src/main/java/de/rpgframework/shadowrun6/commlinkbinary/ComLinkStarter.java
+++ src/main/java/de/rpgframework/shadowrun6/commlinkbinary/ComLinkStarter.java
@@ -1,14 +1,17 @@
 package de.rpgframework.shadowrun6.commlinkbinary;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.logging.LogManager;
 
 import de.rpgframework.eden.client.jfx.EdenSettings;
+import de.rpgframework.genericrpg.LicenseManager;
 import de.rpgframework.shadowrun6.comlink.ComLinkMain;
 
 public class ComLinkStarter {
 
 	public static void main(String[] args) {
+//    	LicenseManager.storeGlobalLicenses(List.of("SHADOWRUN6/CORE","SHADOWRUN6/COMPANION","SHADOWRUN6/FIRING_SQUAD","SHADOWRUN6/STREET_WYRD","SHADOWRUN6/DOUBLE_CLUTCH","SHADOWRUN6/HACK_SLASH"));
 		LogManager.getLogManager().reset();
 		EdenSettings.setupDirectories("CommLink6");
 		Locale.setDefault(EdenSettings.getPreferredLangauge("CommLink6"));
