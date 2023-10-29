e5ef9c5931b2b1a8e5949561b47d89903a900d3c
Stefan Prelle <stefan@rpgframework.de>
2023-10-29 17:50:13 +0100

    With store upload (not working yet)

--- src/main/java/de/rpgframework/shadowrun6/commlinkbinary/ComLinkStarter.java
+++ src/main/java/de/rpgframework/shadowrun6/commlinkbinary/ComLinkStarter.java
@@ -1,11 +1,9 @@
 package de.rpgframework.shadowrun6.commlinkbinary;
 
-import java.util.List;
 import java.util.Locale;
 import java.util.logging.LogManager;
 
 import de.rpgframework.eden.client.jfx.EdenSettings;
-import de.rpgframework.genericrpg.LicenseManager;
 import de.rpgframework.shadowrun6.comlink.ComLinkMain;
 
 public class ComLinkStarter {
